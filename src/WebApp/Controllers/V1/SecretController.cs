5e6642cf79ab6c9f518c24c6ea20d188b7cb709a
Mikhail Kiyan <MKiyan@luxoft.com>
2020-11-04 01:23:27 +0200

    Added ApiKey-based authentication

---
+++ src/WebApp/Controllers/V1/SecretController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPI.Filters;
+
+namespace WebApplicationAPI.Controllers.V1 {
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase {
+        [HttpGet("secret")]
+        public IActionResult GetSecret() {
+            return this.Ok("I have no secret");
+        }
+    }
+}
